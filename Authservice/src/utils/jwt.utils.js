2c757e7b20bc478449de90b99bdcbc3e55961246
divyamkukreti <139796760+divyamkukreti@users.noreply.github.com>
2025-06-12 12:18:47 +0530

    Delete Authservice/src directory

--- Authservice/src/utils/jwt.utils.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' });
-};
-
-module.exports = {
-  generateToken,
-};
